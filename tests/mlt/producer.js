d3886e9c4eaadf322614089d87065345cace8baf
Jeffrey D. Van Alstine <jeffv@redmantech.com>
2011-07-20 11:18:30 -0600

    Updates producer to reflect changes in API Design

--- tests/mlt/producer.js
+++ tests/mlt/producer.js
@@ -1,17 +1,30 @@
 var assert = require('assert')
   , Producer = require('../../lib/mlt/producer')
 
-var p = new Producer
-delete p._attribs.id //ignore the id since it's UUID and unpredictable for the purposes of this test
-
-p.attribs({
-  in: 0,
-  out: 999
+var p = new Producer.Image({
+  length: 100,
+  source: '/home/jeffrey/Downloads/banana.jpg'
 })
-p.props({
-  mlt_service: 'pixbuff',
-  resource: 'apples.jpg'
-})
+p._attribs.id = '9571545a-b072-4a72-9b64-fec67563c1be' //override random number for testing
 
-var xml = '<producer in="0" out="999"><property name="mlt_service" value="pixbuff"/><property name="resource" value="apples.jpg"/></producer>'
-assert.equal(p.toString(),xml, "XML was malformed")
+var xml = '<producer id="9571545a-b072-4a72-9b64-fec67563c1be" mlt_service="pixbuff" out="99" resource="/home/jeffrey/Downloads/banana.jpg"/>'
+assert.equal(p.toString(), xml, "Image Builder Failed")
+
+var a = new Producer.Audio({
+  startFrame: 100,
+  length: 100,
+  source: '/home/jeffrey/Downloads/banana.mp3'
+})
+a._attribs.id = '9571545a-b072-4a72-9b64-fec67563c1be' //override random number for testing
+
+var xml = '<producer id="9571545a-b072-4a72-9b64-fec67563c1be" mlt_service="avformat" in="100" out="199" resource="/home/jeffrey/Downloads/banana.mp3"/>'
+assert.equal(a.toString(), xml, "Audio Builder Failed")
+
+var v = new Producer.Video({
+  length: 100,
+  source: '/home/jeffrey/Downloads/banana.avi'
+})
+v._attribs.id = '9571545a-b072-4a72-9b64-fec67563c1be' //override random number for testing
+
+var xml = '<producer id="9571545a-b072-4a72-9b64-fec67563c1be" mlt_service="avformat" out="99" resource="/home/jeffrey/Downloads/banana.avi"/>'
+assert.equal(v.toString(), xml, "Video Builder Failed")
