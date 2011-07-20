3e9ec8176e8baab2ac580e0a6ee355e60aeb187a
Jeffrey D. Van Alstine <jeffv@redmantech.com>
2011-07-20 11:45:43 -0600

    Updates entry to reflect new api plan. Updated entry test Added a missing var to blank

--- tests/mlt/playlist/entry.js
+++ tests/mlt/playlist/entry.js
@@ -2,17 +2,12 @@ var assert = require('assert')
   , Entry = require('../../../lib/mlt/playlist/entry.js')
 
 var entry = new Entry({
-  in: 0,
-  out: 999,
   producer: {
-    id: function () {return 1} //(go ducktyping)
-  }
+    id: function ()  { return 'abc' } 
+  },
+  startFrame: 100,
+  length: 100
 })
 
-entry.in(999).out(1999).producer({ id: function () {return 2} })
-
-var xml = '<entry in="999" out="1999" producer="2"/>'
-assert.equal(entry.toString(), xml, "XML generation failed")
-assert.equal(entry.in(), 999, "In getter/setter failed")
-assert.equal(entry.out(), 1999, "Out getter/setter failed")
-assert.equal(entry.producer(), 2, "Producer getter/setter failed")
+var xml = '<entry producer="abc" in="100" length="199"/>'
+assert.equal(entry.toString(), xml, "Entry builder failed")
