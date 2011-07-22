d192b8b575919bc4d7784253007e95ffb44ed28f
Jeffrey D. Van Alstine <jeffv@redmantech.com>
2011-07-22 14:27:01 -0600

    Adds updated mlt container class

--- tests/mlt.js
+++ tests/mlt.js
@@ -4,27 +4,6 @@ var assert = require('assert')
 
 var mlt = new MLT
 
-var producers = {}
+var xml = '<?xml version="1.0" encoding="utf-8"?><mlt/>'
 
-var p = new MLT.Producer
-p._attribs.id = 'a' //override random number for testing.
-producers[p.attribs({
-  resource: 'usr/share/xml/docbook/stylesheet/docbook-xsl/images/warning.svg',
-  mlt_service: 'pixbuff'
-}).id()] = p
-
-p = new MLT.Producer
-p._attribs.id = 'b' //override random number for testing.
-producers[p.id()] = p.props({
-  resource: '/home/jeffrey/Music/Doc/Sphinx/01 - Sphinx',
-  mlt_service: 'avformat'
-})
-
-_.each(producers, function(prod) {
-  mlt.producers.push(prod)
-})
-
-
-var xml = '<?xml version="1.0" encoding="utf-8"?><mlt><producer id="a" resource="usr/share/xml/docbook/stylesheet/docbook-xsl/images/warning.svg" mlt_service="pixbuff"/><producer id="b"><property name="resource" value="/home/jeffrey/Music/Doc/Sphinx/01 - Sphinx"/><property name="mlt_service" value="avformat"/></producer></mlt>'
-
-assert.equal(mlt.toString(), xml, 'XML Generation Failed')
+assert.equal(mlt.toString(), xml, 'MLT XML Generation Failed')
