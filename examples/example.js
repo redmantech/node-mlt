1396207f713be1da9865d4dbb821a216da75af54
Jeffrey D. Van Alstine <jeffv@redmantech.com>
2011-07-27 14:50:34 -0600

    Proof of concept / prototype example

--- examples/example.js
+++ examples/example.js
@@ -23,8 +23,11 @@ var getResources = function (type, callback) {
 
 var mlt = new MLT
   , multitrack = new MLT.Multitrack
-  , showTime = 100
   , transitionTime = 25
+  , showTime = 100 + transitionTime * 2
+  , trackNumbers = []
+  , trackNumber = 1
+
 step(
   function () {
     getResources('images', this.parallel())
@@ -47,7 +50,7 @@ step(
     mlt.push(audio)
     var audio = (new MLT.Playlist()).entry({
       producer:audio,
-      length: showTime * images.length
+      length: (images.length * showTime) - (images.length-1) * transitionTime
     })
     mlt.push(audio)
     multitrack.addTrack(new MLT.Multitrack.Track(audio))
@@ -55,67 +58,83 @@ step(
     //Process Images
     _.each(images, function(image, k, images) {
       var cb = group()
-        , playlist = new MLT.Playlist()
-
       gm(image).size(function (err, size) {
         if (err) {
           return cb(err, null)
         }
 
-        image = new MLT.Producer.Image({ source: image })
-        mlt.push(image)
-
-        if (k > 0) {
-          playlist.blank( k * showTime)
-        }
-
-        var scale = {
-          init: 1 + (Math.random() * 0.125),
-          end: 1 + (Math.random() * 0.125)
-        } 
-
-        playlist.entry({
-          producer: image,
-          length: showTime,
-          filters: [
-            (new MLT.Filter.Affine({
-               
-            })).geometry([
-              {
-                frame: 0,
-                x: parseInt(-size.width * 0.25 * scale.init),
-                y: parseInt(-size.height * 0.25 * scale.init),
-                w: parseInt(size.width * scale.init),
-                h: parseInt(size.height * scale.init),
-                sat: 100   
-              },
-              {
-                frame: showTime - 1,
-                x: parseInt(-(0.125 + 0.25*Math.random()) * size.width * scale.end),
-                y: parseInt(-(0.125 + 0.25*Math.random()) * size.height * scale.end),
-                w: parseInt(scale.end * size.width),
-                h: parseInt(scale.end * size.height),
-                sat: 100  
-              }
-            ])
-          ]
+        return cb(null, {
+          source: image,
+          height: size.height,
+          width: size.width
         })
-
-        multitrack.addTrack(new MLT.Multitrack.Track(playlist))
-
-        mlt.push(playlist)
-        cb()
       })
-    }, this)
+    })
   },
-  function (err) {
+  function (err, images) {
     if (err) {
       throw err
     }
 
-    //Throw it all together
-    var tractor = new MLT.Tractor()
+    var tractor = new MLT.Tractor
+
+    _.each(images, function(image, k) {
+      var producer = new MLT.Producer.Image({source: image.source})
+        , playlist = new MLT.Playlist()
+      mlt.push(producer)
+
+      if (k > 0) {
+        playlist.blank(k * showTime - transitionTime * k)
+      }
+
+      var scale = {
+        init: 1 + (Math.random() * 0.125),
+        end: 1 + (Math.random() * 0.125)
+      }
+
+      playlist.entry({
+        producer: producer,
+        length: showTime,
+        filters: [
+          (new MLT.Filter.Affine).geometry([
+            {
+              frame: 0,
+              x: parseInt(-image.width * 0.25 * scale.init),
+              y: parseInt(-image.height * 0.25 * scale.init),
+              w: parseInt(image.width * scale.init),
+              h: parseInt(image.height * scale.init),
+              sat: 100   
+            },
+            {
+              frame: showTime - 1,
+              x: parseInt(-(0.125 + 0.25*Math.random()) * image.width * scale.end),
+              y: parseInt(-(0.125 + 0.25*Math.random()) * image.height * scale.end),
+              w: parseInt(scale.end * image.width),
+              h: parseInt(scale.end * image.height),
+              sat: 100
+            }
+          ])
+        ]
+      })
+
+      mlt.push(playlist)
+      multitrack.addTrack(new MLT.Multitrack.Track(playlist))
+
+      if (images[k+1] !== undefined) { //have something to transition to
+        tractor.push(new MLT.Transition.Luma({
+          start: (k+1) * (showTime - transitionTime),
+          length: transitionTime,
+          to: k+2, //add 1 because music is track 0
+          from: k+1
+        }))
+      }
+    })
+
     mlt.push(tractor.push(multitrack))
-    console.log(mlt.toString({pretty: true}))
+ 
+    var filename = '/home/jeffrey/kdenlive/scripts/script003.sh.mlt'    
+    fs.writeFile(filename, mlt.toString(), function () {
+      console.log('WROTE: ' + filename)
+    })
   }
 )
