dded140efd9b47f352e482033748ba6605f560dd
Jeffrey D. Van Alstine <jeffv@redmantech.com>
2011-07-27 10:33:47 -0600

    Updates entry obj to push filters Updated example script to use filters Updates mlt namespace to reference filters

--- examples/example.js
+++ examples/example.js
@@ -1,6 +1,7 @@
 var fs = require('fs')
-  , step = require('step')
   , _ = require('underscore')
+  , gm = require('gm')
+  , step = require('step')
   , MLT = require('../lib/mlt')
 
 var getResources = function (type, callback) {
@@ -19,6 +20,11 @@ var getResources = function (type, callback) {
   })
 }
 
+
+var mlt = new MLT
+  , multitrack = new MLT.Multitrack
+  , showTime = 100
+  , transitionTime = 25
 step(
   function () {
     getResources('images', this.parallel())
@@ -28,29 +34,7 @@ step(
     if (err) {
       throw err
     }
-    var mlt = new MLT
-      , showTime = 100
-      , multitrack = new MLT.Multitrack
-
-    //Process Images
-    _.each(images, function(image, k, images) {
-      var playlist = new MLT.Playlist()
-
-      image = new MLT.Producer.Image({ source: image }) 
-      mlt.push(image)
-
-      if (k > 0) {
-        playlist.blank( k * showTime)
-      }
-      playlist.entry({
-        producer: image,
-        length: showTime
-      })
-
-      multitrack.addTrack(new MLT.Multitrack.Track(playlist))
-
-      mlt.push(playlist)
-    })
+    var group = this.group();
 
     //Process Audio
     audio = new MLT.Producer.Audio({
@@ -67,11 +51,71 @@ step(
     })
     mlt.push(audio)
     multitrack.addTrack(new MLT.Multitrack.Track(audio))
+  
+    //Process Images
+    _.each(images, function(image, k, images) {
+      var cb = group()
+        , playlist = new MLT.Playlist()
+
+      gm(image).size(function (err, size) {
+        if (err) {
+          return cb(err, null)
+        }
+
+        image = new MLT.Producer.Image({ source: image })
+        mlt.push(image)
+
+        if (k > 0) {
+          playlist.blank( k * showTime)
+        }
+
+        var scale = {
+          init: 1 + (Math.random() * 0.125),
+          end: 1 + (Math.random() * 0.125)
+        } 
+
+        playlist.entry({
+          producer: image,
+          length: showTime,
+          filters: [
+            (new MLT.Filter.Affine({
+               
+            })).geometry([
+              {
+                frame: 0,
+                x: parseInt(-size.width * 0.25 * scale.init),
+                y: parseInt(-size.height * 0.25 * scale.init),
+                w: parseInt(size.width * scale.init),
+                h: parseInt(size.height * scale.init),
+                sat: 100   
+              },
+              {
+                frame: showTime - 1,
+                x: parseInt(-(0.125 + 0.25*Math.random()) * size.width * scale.end),
+                y: parseInt(-(0.125 + 0.25*Math.random()) * size.height * scale.end),
+                w: parseInt(scale.end * size.width),
+                h: parseInt(scale.end * size.height),
+                sat: 100  
+              }
+            ])
+          ]
+        })
+
+        multitrack.addTrack(new MLT.Multitrack.Track(playlist))
+
+        mlt.push(playlist)
+        cb()
+      })
+    }, this)
+  },
+  function (err) {
+    if (err) {
+      throw err
+    }
 
     //Throw it all together
     var tractor = new MLT.Tractor()
     mlt.push(tractor.push(multitrack))
-
     console.log(mlt.toString({pretty: true}))
   }
 )
