bfb2a00eb395596db98fb765ec10505f19886461
Jeffrey D. Van Alstine <jeffv@redmantech.com>
2012-03-28 10:22:52 -0600

    Updates runtests script to run all tests

--- tests/runtests.js
+++ tests/runtests.js
@@ -6,16 +6,36 @@ require('./interfaces/xml.js')
 console.log('>>> PROPERTIES INTERFACE <<<')
 require('./interfaces/properties.js')
 
-console.log('### MLT PROPERTY ###')
-require('./mlt/property.js')
-
-console.log('### MLT PRODUCER ###')
-require('./mlt/producer.js')
+console.log('### MLT MULTITRACK::TRACK ###')
+require('./mlt/multitrack/track.js')
 
 console.log('### MLT PLAYLIST::BLANK ###')
 require('./mlt/playlist/blank.js')
 
 console.log('### MLT PLAYLIST::ENTRY ###')
 require('./mlt/playlist/entry.js')
+
+console.log('### MLT FILTER ###')
+require('./mlt/filter.js')
+
+console.log('### MLT MULTITRACK ###')
+require('./mlt/multitrack.js')
+
+console.log('### MLT PLAYLIST ###')
+require('./mlt/playlist.js')
+
+console.log('### MLT PRODUCER ###')
+require('./mlt/producer.js')
+
+console.log('### MLT PROPERTY ###')
+require('./mlt/property.js')
+
+console.log('### MLT TRACTOR ###')
+require('./mlt/tractor.js')
+
+console.log('### MLT TRANSITION ###')
+require('./mlt/TRANSITION.js')
+
+
 
 console.log('*** END TESTS ***')
