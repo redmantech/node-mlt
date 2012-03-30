c72540b373ea905e9c01155a8bc6201f1827baf9
Jeffrey D. Van Alstine <jeffv@redmantech.com>
2012-03-30 11:11:44 -0600

    Adds ability to pass ffmpeg options to melt

--- examples/config.example.js
+++ examples/config.example.js
@@ -1,6 +1,15 @@
 module.exports = {
   flickr: {
     apiKey: 'abcdef0123456789abcdefabcdef0123456789abcdefabcdef0123456789abcdefabcdef0123456789abcdefabcdef0123456789abcdefabcdef0123456789abcdefabcdef0123456789abcdefabcdef0123456789abcdefabcdef0123456789abcdef'
+  },
+  ffmpeg: {
+    options: {
+      'r': '25',
+      'g': '1000',
+      's': 'vga',
+      'b': '64k',
+      'vcodec': 'libx264'
+    }
   }
 }
 
--- examples/example.js
+++ examples/example.js
@@ -181,6 +181,12 @@ step(
       , videoFilename = './images/untitled.mp4'
       , child = 'melt ' + mltFilename + ' -consumer avformat:' + videoFilename;
 
+    if (config.ffmpeg && config.ffmpeg.options) {
+      _.each(config.ffmpeg.options, function (value, key) {
+        child += ' ' + key + '=' + value;
+      });
+    }
+
     console.log('Melting. Please be Patient!');
     child = child_process.exec(child, function (err, stdout, stderr) { 
       if (err) {
@@ -190,154 +196,4 @@ step(
       console.log('Finished: ' + videoFilename);
     });
   }
-)
-
-/*
-var getResources = function (type, callback) {
-  var dir = '/' + type + '/'
-
-  fs.readdir('.' + dir, function(err, resources) {
-    if (err) {
-      return callback(err)
-    }
-
-    resources = _.map(resources.sort(), function (resource) {
-      return process.cwd() + dir + resource
-    })
-
-    callback(null, resources);
-  })
-}
-
-
-var mlt = new MLT
-  , multitrack = new MLT.Multitrack
-  , transitionTime = 25
-  , showTime = 100 + transitionTime * 2
-  , trackNumbers = []
-  , trackNumber = 1
-
-step(
-  function () {
-    getResources('images', this.parallel())
-    getResources('audio', this.parallel())
-  },
-  function (err, images, audio) {
-    if (err) {
-      throw err
-    }
-    var group = this.group();
-
-    //Process Audio
-    audio = new MLT.Producer.Audio({
-      source: _.first(
-        _.sortBy(audio, function () {
-          return Math.random()
-        })
-      )
-    })
-    mlt.push(audio)
-    var audio = (new MLT.Playlist()).entry({
-      producer:audio,
-      length: (images.length * showTime) - (images.length-1) * transitionTime
-    })
-    mlt.push(audio)
-    multitrack.addTrack(new MLT.Multitrack.Track(audio))
-  
-    //Process Images
-    _.each(images, function(image, k, images) {
-      var cb = group()
-      gm(image).size(function (err, size) {
-        if (err) {
-          return cb(err, null)
-        }
-
-        return cb(null, {
-          source: image,
-          height: size.height,
-          width: size.width
-        })
-      })
-    })
-  },
-  function (err, images) {
-    if (err) {
-      throw err
-    }
-
-    var tractor = new MLT.Tractor
-
-    _.each(images, function(image, k) {
-      var producer = new MLT.Producer.Image({source: image.source})
-        , playlist = new MLT.Playlist()
-      mlt.push(producer)
-
-      if (k > 0) {
-        playlist.blank(k * showTime - transitionTime * k)
-      }
-
-      var scale = {
-        init: 1 + (Math.random() * 0.125),
-        end: 1 + (Math.random() * 0.125)
-      }
-
-      playlist.entry({
-        producer: producer,
-        length: showTime,
-        filters: [
-          (new MLT.Filter.Affine).geometry([
-            {
-              frame: 0,
-              x: parseInt(-image.width * 0.25 * scale.init),
-              y: parseInt(-image.height * 0.25 * scale.init),
-              w: parseInt(image.width * scale.init),
-              h: parseInt(image.height * scale.init),
-              sat: 100   
-            },
-            {
-              frame: showTime - 1,
-              x: parseInt(-(0.125 + 0.25*Math.random()) * image.width * scale.end),
-              y: parseInt(-(0.125 + 0.25*Math.random()) * image.height * scale.end),
-              w: parseInt(scale.end * image.width),
-              h: parseInt(scale.end * image.height),
-              sat: 100
-            }
-          ])
-        ]
-      })
-
-      mlt.push(playlist)
-      multitrack.addTrack(new MLT.Multitrack.Track(playlist))
-
-      if (images[k+1] !== undefined) { //have something to transition to
-        tractor.push(new MLT.Transition.Luma({
-          start: (k+1) * (showTime - transitionTime),
-          length: transitionTime,
-          to: k+2, //add 1 because music is track 0
-          from: k+1
-        }))
-      }
-    })
-
-    mlt.push(tractor.push(multitrack))
- 
-    var filename = '/home/jeffrey/kdenlive/scripts/script003.sh.mlt'    
-    fs.writeFile(filename, mlt.toString(), function () {
-      console.log('WROTE: ' + filename)
-      console.log('Compiling ...')
-
-      var spawn = require('child_process').spawn
-
-      compiler = spawn('/home/jeffrey/kdenlive/scripts/script003.sh', [])
-      compiler.on('exit', function (code) {
-        if (code === 0){
-          console.log('... Finis!')
-        }
-        else {
-          console.log('Something Funny Happened (Code ' + code + ')')
-        }
-      })
-    })
-  }
-)
-*/
+);
